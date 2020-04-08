68e9eefaec28580aa2b75ba6a9dc6d92448fe3ba
Kai Romero <jorge.romero.ortega@gmail.com>
2020-04-08 18:47:27 +0200

    nuevas imagenes y comienzo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,64 @@
-import React from "react"
+import React, {Fragment} from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+//import Layout from "../components/layout"
+//import Image from "../components/image"
 import SEO from "../components/seo"
+import background from "../images/background.jpg"
 
-const doFetch = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-}
+
+const Image = styled.div`
+   width: 100%;
+   height: 100vh;
+   background-image: url(${background});
+   background-size: cover;
+   background-position: center;
+   position: absolute;
+   z-index: -1;
+`
+
+const Titulo = styled.div`
+   color: white;
+   font-weight: 200;
+   font-family: sans-serif;
+   font-size: 2em;
+   padding-bottom: 10px;
+   line-height: 1em;
+`
+
+const Main = styled.section`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  text-align: center;
+  padding: 40px;
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
+  font-family: Roboto;
+  font-size: 1em;
+  line-height: 1em;
+  a {
+    margin-top: 10px;
+    color: white;
+  }
+  @media (max-width: 400px) {
+    width: 90% !important;
+    background: red;
+  }
+`
 
 const IndexPage = () => (
-  <Layout>
+  <Fragment>
     <SEO title="Home" />
-    ES EL MASTER DE LA PRUEBA
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <button onClick={doFetch}>Un boton</button>
-  </Layout>
+    <Image />
+    <Main>
+      <Titulo>Blog design for Music</Titulo>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Main>
+  </Fragment>
 )
 
 export default IndexPage
